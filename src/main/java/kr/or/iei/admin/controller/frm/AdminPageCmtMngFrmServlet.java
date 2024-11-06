5ce4efeaa933cfe35054570a34263f9a67c8d3d0
엄태호 <taeho.eom001@gmail.com>
2024-11-06 18:55:17 +0900

    파일명 Frm 및 Notice->Post 수정

--- src/main/java/kr/or/iei/admin/controller/frm/AdminPageCmtMngFrmServlet.java
+++ src/main/java/kr/or/iei/admin/controller/frm/AdminPageCmtMngFrmServlet.java
@@ -16,13 +16,13 @@ import kr.or.iei.comment.vo.Comment;
  * Servlet implementation class AdminPageCmtMngServlet
  */
 @WebServlet("/admin/adminCmtFrm")
-public class AdminPageCmtMngServlet extends HttpServlet {
+public class AdminPageCmtMngFrmServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public AdminPageCmtMngServlet() {
+	public AdminPageCmtMngFrmServlet() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
